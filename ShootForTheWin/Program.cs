73065af4804e05b1bfc30cff6c2696c5f1320b28
Alexander Agov <164613746+alexander-agov@users.noreply.github.com>
2024-05-21 11:47:39 +0300

    Add files via upload

---
+++ ShootForTheWin/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace ShootForTheWin
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            List<int> numbers = Console.ReadLine().Split(" ").Select(int.Parse).ToList();
+            string input;
+            int shootedTargets = 0;
+            while ((input = Console.ReadLine()) != "End")
+            {
+                int targetIndex = int.Parse(input);
+                if (targetIndex < 0 || targetIndex > numbers.Count - 1)
+                {
+                    continue;
+                }
+                int targetValue = numbers[targetIndex];
+                numbers[targetIndex] = -1;
+                shootedTargets++;
+                for (int i = 0; i < numbers.Count; i++)
+                {
+                    if (numbers[i] == -1)
+                    {
+                        continue;
+                    }
+                    else if (numbers[i] > targetValue)
+                    {
+                        numbers[i] -= targetValue;
+                    }
+                    else if (numbers[i] <= targetValue)
+                    {
+                        numbers[i] += targetValue;
+                    }
+                }
+            }
+            Console.WriteLine($"Shot targets: {shootedTargets} -> {string.Join(" ", numbers)}");
+        }
+    }
+}
