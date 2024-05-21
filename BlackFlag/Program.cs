83def7e34255498ec0db0609760ee04aaecffc8d
Alexander Agov <164613746+alexander-agov@users.noreply.github.com>
2024-05-21 11:26:45 +0300

    Add files via upload

---
+++ BlackFlag/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace BlackFlag
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int days = int.Parse(Console.ReadLine());
+            int dailyPlunder = int.Parse(Console.ReadLine());
+            double expectedPlunder = double.Parse(Console.ReadLine());
+            double total = 0;
+            for (int day = 1; day <= days; day++)
+            {
+                total += (double)dailyPlunder;
+                if (day % 3 == 0)
+                {
+                    total += dailyPlunder * 0.5;
+                }
+                if (day % 5 == 0)
+                {
+                    total *= 0.7;
+                }
+            }
+            if (expectedPlunder <= total)
+            {
+                Console.WriteLine($"Ahoy! {total:f2} plunder gained.");
+            }
+            else
+            {
+                double percentage = total / expectedPlunder * 100;
+                Console.WriteLine($"Collected only {percentage:f2}% of the plunder.");
+            }
+        }
+    }
+}
