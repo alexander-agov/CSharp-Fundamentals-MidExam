f643238ce6f6f5181cfa2bda224621b065c0ed76
Alexander Agov <164613746+alexander-agov@users.noreply.github.com>
2024-05-21 11:31:06 +0300

    Add files via upload

---
+++ CounterStrike/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace CounterStrike
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int initialEnergy = int.Parse(Console.ReadLine());
+            string anyDistance = "";
+            int countWonBattles = 0;
+            while (true)
+            {
+                anyDistance = Console.ReadLine();
+                if (anyDistance == "End of battle")
+                {
+                    Console.WriteLine($"Won battles: {countWonBattles}. Energy left: {initialEnergy}");
+                    return;
+                }
+                if (int.Parse(anyDistance) <= initialEnergy)
+                {
+                    initialEnergy -= int.Parse(anyDistance);
+                    countWonBattles++;
+                    if (countWonBattles % 3 == 0)
+                    {
+                        initialEnergy += countWonBattles;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine($"Not enough energy! Game ends with {countWonBattles} won battles and {initialEnergy} energy");
+                    return;
+                }
+            }
+        }
+    }
+}
