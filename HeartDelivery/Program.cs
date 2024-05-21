9d3e34898000a2a448894b1137dd732d64fdabcf
Alexander Agov <164613746+alexander-agov@users.noreply.github.com>
2024-05-21 11:38:54 +0300

    Add files via upload

---
+++ HeartDelivery/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace HeartDelivery
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            List<int> evenNumbers = Console.ReadLine().Split("@").Select(int.Parse).ToList();
+            string input;
+            int currentIndex = 0;
+            while ((input = Console.ReadLine()) != "Love!")
+            {
+                string[] command = input.Split();
+                int jumpLength = int.Parse(command[1]);
+                currentIndex += jumpLength;
+                if (currentIndex >= evenNumbers.Count)
+                {
+                    currentIndex = 0;
+                }
+                if (evenNumbers[currentIndex] == 0)
+                {
+                    Console.WriteLine($"Place {currentIndex} already had Valentine's day.");
+                }
+                else
+                {
+                    evenNumbers[currentIndex] -= 2;
+                    if (evenNumbers[currentIndex] == 0)
+                    {
+                        Console.WriteLine($"Place {currentIndex} has Valentine's day.");
+                    }
+                }
+            }
+            Console.WriteLine($"Cupid's last position was {currentIndex}.");
+            if (evenNumbers.All(x => x == 0))
+            {
+                Console.WriteLine($"Mission was successful.");
+            }
+            else
+            {
+                int houseCount = evenNumbers.Count(x => x > 0);
+                Console.WriteLine($"Cupid has failed {houseCount} places.");
+            }
+        }
+    }
+}
