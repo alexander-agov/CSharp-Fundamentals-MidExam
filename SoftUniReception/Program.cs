f4c26cd7fd10871b137a08ba8d779455c46ae090
Alexander Agov <164613746+alexander-agov@users.noreply.github.com>
2024-05-21 11:49:17 +0300

    Add files via upload

---
+++ SoftUniReception/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace SoftUniReception
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int employeeOne = int.Parse(Console.ReadLine());
+            int employeeTwo = int.Parse(Console.ReadLine());
+            int employeeThree = int.Parse(Console.ReadLine());
+            int students = int.Parse(Console.ReadLine());
+            int allStudentsPerHour = employeeOne + employeeTwo + employeeThree;
+            int restStudents = 0;
+            while (students > 0)
+            {
+                students -= allStudentsPerHour;
+                restStudents++;
+                if (restStudents % 4 == 0)
+                {
+                    restStudents++;
+                }
+            }
+            Console.WriteLine($"Time needed: {restStudents}h.");
+        }
+    }
+}
+
